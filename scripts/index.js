894b76eef98ebdca6b33d35508c9b33de5bc532e
Zinnurov Ramil <zinnurov.ramil@infotecs.ru>
2023-12-15 23:11:12 +0500

    Вывел карточки в цикле

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,17 @@
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
-const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-placesList.append(cardElement)
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__image').src = initialCards[i].link;
+  cardElement.querySelector('.card__title').innerText = initialCards[i].name;
+  placesList.append(cardElement)
+
+}
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
