f8f3cc8addc62c6731e026e46f6dac15b97b67b4
Zinnurov Ramil <zinnurov.ramil@infotecs.ru>
2023-12-15 22:57:11 +0500

    Добавил карточку на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
+const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
-
+placesList.append(cardElement)
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
