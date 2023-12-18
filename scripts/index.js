f4809c7b8b7ccbd7643ed78d91fe3b61d398a141
Zinnurov Ramil <zinnurov.ramil@infotecs.ru>
2023-12-18 23:05:30 +0500

    Добавил удаление карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,25 @@
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-const placesList = document.querySelector('.places__list');
+const placesList = document.querySelector(".places__list");
+
 // @todo: Функция создания карточки
+// @todo: Вывести карточки на страницу
 
 for (let i = 0; i < initialCards.length; i++) {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  cardElement.querySelector('.card__image').src = initialCards[i].link;
-  cardElement.querySelector('.card__title').innerText = initialCards[i].name;
-  placesList.append(cardElement)
-
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = initialCards[i].link;
+  cardElement.querySelector(".card__title").textContent = initialCards[i].name;
+  placesList.append(cardElement);
 }
+
 // @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+const deleteButton = document.querySelectorAll(".card__delete-button");
+
+deleteButton.forEach(function (item) {
+  item.addEventListener("click", function (event) {
+    event.target.parentElement.remove();
+  });
+});
