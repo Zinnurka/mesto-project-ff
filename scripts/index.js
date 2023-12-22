bd3b9310044f2ee8d53ba2a7a8df44775b70de88
Zinnurov Ramil <zinnurov.ramil@infotecs.ru>
2023-12-22 09:27:45 +0500

    Лучше назвать как cardsContainer

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@
 const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
-const placesList = document.querySelector(".places__list");
+const cardsContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 // @todo: Вывести карточки на страницу
@@ -11,7 +11,7 @@ for (let i = 0; i < initialCards.length; i++) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   cardElement.querySelector(".card__image").src = initialCards[i].link;
   cardElement.querySelector(".card__title").textContent = initialCards[i].name;
-  placesList.append(cardElement);
+  cardsContainer.append(cardElement);
 }
 
 // @todo: Функция удаления карточки
